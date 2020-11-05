740bf11fe60f6072c8d5c54cb2bc4a973f33b556
Hana <67531901+hajhana@users.noreply.github.com>
2020-11-04 23:35:10 -0500

    Add files via upload

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,23 @@
-
-
-
 const request = require('request');
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log(typeof body);
-  if (error) {
-    console.log(`Erorr: ${error}`);
-  } else {
-    let data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Error: requested breed is not found');
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q='+breedName, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log(typeof body);
+    if (error) {
+      console.log(`Erorr: ${error}`);
     } else {
-      console.log(`\n Breed Description: \n${data[0].description}\n`);
+      let data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log('Error: requested breed is not found');
+      } else {
+        console.log(`\n Breed Description: \n${data[0].description}\n`);
+      }
     }
-  }
-});
+  });
+};
 
 
+module.exports = fetchBreedDescription;
